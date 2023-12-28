3317b8d6a611003335d8e10eda61b3ee60131712
Shaqmonster <87325561+Shaqmonster@users.noreply.github.com>
2023-12-29 01:10:13 +0530

    Update productTable.tsx

--- components/Tables/productTable.tsx
+++ components/Tables/productTable.tsx
@@ -136,14 +136,15 @@ const ProductsTable = (props: { Products: Product[] }) => {
         </h4>
       </div>
 
+
       <div className="grid grid-cols-12 md:grid-cols-10 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 sm:bg-red md:px-4 2xl:px-7.5">
         <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
+          <p className="font-medium ">Product Name</p>
         </div>
         <div className="col-span-2 hidden items-center sm:flex ">
           <p className="font-medium">Category</p>
         </div>
-        <div className="col-span-2 flex items-center">
+        <div className="col-span-2 flex items-center ">
           <p className="font-medium pr-2">Description</p>
         </div>
         <div className="col-span-1 flex items-center">
@@ -171,7 +172,7 @@ const ProductsTable = (props: { Products: Product[] }) => {
               <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
                 <div className="h-12.5 w-15 rounded-md">
                   <Image
-                    src={product.image}
+                    src={product.image1}
                     width="56"
                     height="50"
                     className="w-full h-full rounded-md"
